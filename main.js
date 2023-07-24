83aacc7b6454ae2bb67ce3f61861d150642592cc
Ericdid <ericdidom@gmail.com>
2023-07-24 13:53:37 +0200

    add.loop

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 // Sovrascrivo la lista del HTML in Javascript
 const Mylist = document.getElementById(`my-list`);
 // inizio ciclo
-for (let i = 1; i <= 100; i++) {
-  console.log(i);
-  Mylist.innerHTML += `<li>${i}</li>`;
-  //   prima variabile del ciclo (Fizz {3})
-
-  // seconda variabile (buzz (5))
-
-  // terza variabile (fizzbuzz (3/5))
+for (let i = 1; i < 101; i++) {
+  if (i % 15 == 0) console.log("fizzbuzz");
+  else if (i % 5 == 0) console.log("buzz");
+  else if (i % 3 == 0) console.log("fizz");
+  else console.log(i);
 }
